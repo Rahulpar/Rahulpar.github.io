29d13a78d8112aab4c3c950b64eca92bf3612128
Rahul Parakkat <Rahul.Parakkat@sjrb.ca>
2020-03-10 20:17:50 -0600

    secure access

--- js/store.js
+++
@@ -1,7 +0,0 @@
-console.log("here");
-$(".jumbotron").css({ height: $(window).height() + "px" });
-
-$(window).on("resize", function() {
-  $(".jumbotron").css({ height: $(window).height() + "px" });
-});
-
